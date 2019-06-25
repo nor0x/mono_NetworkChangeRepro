e7777a7e952c62e0ad1b5061362854835dafbd9d
nor0x <nor0x@users.noreply.github.com>
2019-06-25 15:49:11 +0200

    initial commit

---
+++ NetworkChangeRepro/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NetworkChangeRepro
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void NetworkChange_NetworkAddressChanged(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("changed");
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            try
+            {
+                NetworkChange.NetworkAddressChanged -= NetworkChange_NetworkAddressChanged;
+                NetworkChange.NetworkAddressChanged += NetworkChange_NetworkAddressChanged;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
+        }
+    }
+}
